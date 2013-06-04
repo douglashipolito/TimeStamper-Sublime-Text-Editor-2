6fceaaa1b51ff200fed3a9a6c5793d9de59663bc
Magno Biét <magno.biet@gmail.com>
2013-06-04 00:23:36 -0300

    Linter

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-import sublime, sublime_plugin, datetime, re
+import sublime
+import sublime_plugin
+import datetime
+import re
 
-class TimeStamper(sublime_plugin.EventListener):        
+
+class TimeStamper(sublime_plugin.EventListener):
 
     def on_pre_save(self, view):
 
         if not view.is_dirty():
             return
 
-        self.settings = sublime.load_settings("TimeStamper.sublime-settings")
-        timestamp_str = datetime.datetime.now().strftime("/* %Y%m%d-%H%M */")
+        self.settings = sublime.load_settings('TimeStamper.sublime-settings')
+        timestamp_str = datetime.datetime.now().strftime('/* %Y%m%d-%H%M */')
         edit = view.begin_edit()
 
         acceptedPath = False
@@ -19,17 +23,16 @@ class TimeStamper(sublime_plugin.EventListener):
             if path == '*':
                 acceptedPath = True
                 break
-            rgPath = re.compile(path, re.IGNORECASE|re.DOTALL)
+            rgPath = re.compile(path, re.IGNORECASE | re.DOTALL)
             rPath = rgPath.search(view.file_name())
             if rPath:
                 acceptedPath = True
                 break
-        
+
         #Is Not Accepted Path
         if not acceptedPath:
-           return ''
+            return ''
 
-        #
         for ext in self .settings.get('accepted_extensions'):
             if view.file_name().endswith('.' + ext):
                 acceptedExtension = True
@@ -40,23 +43,22 @@ class TimeStamper(sublime_plugin.EventListener):
 
             #Regex for verify Exists TimeStamp
             sTimeStamp = '(\\/\\*[\\d\\D]*?\\*\\/)'
-            rg = re.compile(sTimeStamp, re.IGNORECASE|re.DOTALL)
+            rg = re.compile(sTimeStamp, re.IGNORECASE | re.DOTALL)
             m = rg.search(view.substr(view.line(0)))
 
             #Exists, Replace this
             if m:
                 #Regex Blank Spaces after TimeStamp
-                rgSpaces = view.find(sTimeStamp + "+(\\n)",0)
+                rgSpaces = view.find(sTimeStamp + '+(\\n)', 0)
                 erSpaces = re.search('(\\n\\n)', view.substr(view.full_line(rgSpaces)))
-                
+
                 #No Blank Spaces, Add Blank Space before TimeStamp
                 if erSpaces is None:
-                    timestamp_str = timestamp_str + "\n"
-                #Replace    
+                    timestamp_str = timestamp_str + '\n'
+                #Replace
                 view.replace(edit, view.line(0), timestamp_str)
             else:
                 #Add
-                view.insert(edit, 0, timestamp_str + "\n\n")
-        
-        
+                view.insert(edit, 0, timestamp_str + '\n\n')
+
         view.end_edit(edit)
